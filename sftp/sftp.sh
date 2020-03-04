7df12c86a64659d554954a2f701029187e8bfa2f
christianvanos <47158164+christianvanos@users.noreply.github.com>
2020-03-04 15:07:55 +0100

    some fixes

--- sftp/sftp.sh
+++ sftp/sftp.sh
@@ -16,9 +16,9 @@ PASSWORD=$(date +%s | sha224sum | base64 | head -c 10);
 
 while getopts ":u:d:" opt; do
         case $opt in
-                u) USERNAME="$OPTARG";;         # SFTP USERNAME         REQUIRED
-                d) DIRECTORY="$OPTARG";;        # CONTAINER DIRECTORY   REQUIRED
-                \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+                u) USERNAME="$OPTARG";;		# SFTP USERNAME		REQUIRED
+                d) DIRECTORY="$OPTARG";;	# CONTAINER DIRECTORY	REQUIRED
+		\?) echo "Invalid option -$OPTARG" >&2; exit 1;;
                 :) echo "Missing option argument for -$OPTARG">&2; exit 1;;
                 *) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
         esac
@@ -32,30 +32,30 @@ if [[ $EXIT == true ]]; then exit 1; fi
 ######### Functions  ########
 
 pw() {
-        sudo printf "SFTP\n$USERNAME => $PASSWORD" >> ~/pw-sftp;
-        sudo echo "Password: $PASSWORD";
+	sudo printf "SFTP\n$USERNAME => $PASSWORD" >> ~/pw-sftp;
+	sudo echo "Password: $PASSWORD";
 }
 
 addnewuser() {
-        sudo adduser $USERNAME;
-        sudo mkdir -p $DIRECTORY/$USERNAME;
+	sudo adduser $USERNAME;
+	sudo mkdir -p $DIRECTORY/$USERNAME;
+	
+	sudo chown root:root $DIRECTORY;
+	sudo chmod 755 $DIRECTORY;
+	
+	sudo chown $USERNAME:$USERNAME $DIRECTORY/$USERNAME;
+	sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak;
+	printf "Match User $USERNAME\nForceCommand internal-sftp\nPasswordAuthentication yes\nChrootDirectory $DIRECTORY\nPermitTunnel no\nAllowAgentForwarding no\nAllowTcpForwarding no\nX11Forwarding no\n" >> /etc/ssh/sshd_config;
 
-        sudo chown root:root $DIRECTORY;
-        sudo chmod 755 $DIRECTORY;
+	sudo systemctl restart sshd;
 
-        sudo chown $USERNAME:$USERNAME $DIRECTORY/$USERNAME;
-        sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak;
-        printf "Match User $USERNAME\nForceCommand internal-sftp\nPasswordAuthentication yes\nChrootDirectory $DIRECTORY\nPermitTunnel no\nAllowAgentForwarding no\nAllowTcpForwarding no\nX11Forwarding no\n" >> /etc/ssh/sshd_config;
+	sudo echo "====================================";
+	sudo echo "TEST SSH... we should not get access";
+	sudo ssh $USERNAME@localhost;
 
-        sudo systemctl restart sshd;
-
-        sudo echo "====================================";
-        sudo echo "TEST SSH... we should not get access";
-        sudo ssh $USERNAME@localhost;
-
-        sudo echo "====================================";
-        sudo echo "TEST SFTP... we should get access";
-        sudo sftp $USERNAME@localhost;
+	sudo echo "====================================";
+	sudo echo "TEST SFTP... we should get access";
+	sudo sftp $USERNAME@localhost;
 }
 
 ######### Script ########
